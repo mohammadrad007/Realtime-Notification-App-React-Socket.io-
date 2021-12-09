ae2bf43bcf9ebf8a4cec2193fdb0a97b2d0a1570
mohammad mohammadi <mohammad.mohamadi0101@gmail.com>
2021-12-09 23:00:40 +0330

    notif-box-read-notif

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,6 +9,7 @@ interface NavbarType {
 
 const Navbar: React.FC<NavbarType> = ({ socket, user }: NavbarType) => {
   const [notifications, setNotifications] = useState<Array<any>>([]);
+  const [open, setOpen] = useState<Boolean>(false);
 
   useEffect(() => {
     socket.on("getNotification", (data: any) => {
@@ -16,25 +17,53 @@ const Navbar: React.FC<NavbarType> = ({ socket, user }: NavbarType) => {
     });
   }, [socket]);
 
-  console.log(notifications);
+  const displayNotification = ({ senderName, type }: any) => {
+    let action;
+
+    if (type === 1) {
+      action = "liked";
+    } else if (type === 2) {
+      action = "commented";
+    } else {
+      action = "shared";
+    }
+    return (
+      <span
+        className={classes.notification}
+      >{`${senderName} ${action} your post`}</span>
+    );
+  };
+
+  const handleRead = () => {
+    setNotifications([]);
+    setOpen(false);
+  };
 
   return (
     <div className={classes.navbar}>
       <span className={classes.logo}>Lama App</span>
       <p>Hi {user}</p>
       <div className={classes.icons}>
-        <div className={classes.icon}>
+        <div className={classes.icon} onClick={() => setOpen(!open)}>
           <IoNotificationsSharp fontSize={25} />
-          <div className={classes.counter}>2</div>
+          {notifications.length > 0 && (
+            <div className={classes.counter}>{notifications.length}</div>
+          )}
         </div>
         <div className={classes.icon}>
           <IoChatbox fontSize={25} />
-          <div className={classes.counter}>2</div>
         </div>
         <div className={classes.icon}>
           <IoSettings fontSize={25} />
-          <div className={classes.counter}>2</div>
         </div>
+        {open && (
+          <div className={classes.notifications}>
+            {notifications.map((notif) => displayNotification(notif))}
+            <button className={classes.notificationButton} onClick={handleRead}>
+              Mark as read
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
